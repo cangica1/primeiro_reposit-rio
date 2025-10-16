dfe05eb06dd7c690184e53438926d40d380b8234
ꬶɪⱺᥳ𐐼ꬻ𐐼 <adorasuprema@gmail.com>
2025-10-16 15:08:56 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,10 @@
+salario = 100000
+maquiagem = 200
+roupas = 1500
+acessórios = 300
+higiene = 750 
+
+total_gasto = maquiagem + roupas + acessórios + higiene
+restante = salario - total_gasto
+
+print(f"sobrouS: {restante}")
